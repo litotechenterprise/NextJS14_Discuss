bd5cfc52bf33afc0cb7ab1bab6fb8688fff56488
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2024-01-08 18:36:01 -0500

    Implemented header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/Profile";
-export default async function Home() {
-  const session = await auth();
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-
-      {session?.user ? <div>Signed in</div> : <div>Signed out</div>}
-
-      <Profile />
-    </div>
-  );
+export default function Home() {
+  return <div>Home Page</div>;
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,32 +4,25 @@ import {
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
 } from "@nextui-org/react";
 import Link from "next/link";
-import { auth } from "@/auth";
-
-export default async function Header() {
-  const session = await auth();
-
+import HeaderAuth from "./header-auth";
+export default function Header() {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
         <Link href={"/"} className="font-bold">
           Discuss
         </Link>
-        <NavbarContent justify="center">
-          <NavbarItem>
-            <Input />
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem>
-            {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
-          </NavbarItem>
-        </NavbarContent>
       </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
